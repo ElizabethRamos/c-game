27dfb308beb40465f67d8dcc6718dc88276680ed
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-04-04 20:36:36 +0300

    Verifica se há um vencedor

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -19,24 +19,71 @@ int main(int argc, char *argv[])
 
     int cont;
 
+    //inicializa a matriz
     init_matrix();
 
-    do
-    {
-        //chama a funcao para mostrar a matriz
+    do{ 
+        do
+        {
+            //chama a funcao para mostrar a matriz
+            show_matrix();
+
+            //obtem o movimento do jogador
+            get_player_action();
+
+            //verifica se ha um vencedor 
+            if(check_winner() == 1)
+            {
+                winner = 'C';
+                break;
+            }
+
+            get_computer_action();
+
+            if(check_winner() == 1)
+            {
+                winner = 'P';
+                break;
+            }
+
+            //funcao que verifica se houve empate
+            if(check_draw() == 1)
+            {
+                winner = 'E';
+                break;
+            }
+        }
+        while(1);
+
         show_matrix();
 
-        //obtem o movimento do jogador
-        get_player_action();
-
-        //verifica se ha um vencedor 
-        if(check_winner() == 1)
+        //verifica quem venceu
+        if(winner == 'P')
         {
-            winner = 'P';
-            break;
+            printf("\n\nParabéns, você venceu");
+        }
+        else if(winner == 'C')
+        {
+            printf("\n\nVocê perdeu...");
+        }
+        else
+        {
+            printf("\n\nEmpate");
         }
 
-        get_computer_action();
+        printf("\n\nDeseja continuar jogando? <1>SIM <outro valor>NÃO: ");
+        scanf("%d%*c", &cont);
+
+        if(cont == 1)
+        {
+            system("clear");
+            init_matrix();
+            continue;
+        }
+        else 
+        {
+            break;
+        }
     }
     while(1);
 
@@ -122,6 +169,55 @@ void show_matrix()
 
 //funcao que verifica o vencedor 
 int check_winner()
+{
+    int i;
+
+    //verifica as linhas
+    for(i = 0; i < 3; i++)
+    {
+        if(matrix[i][0] != ' ' && matrix[i][1] != ' ' && matrix[i][2] != ' ')
+        {
+            if(matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2])
+            {
+                return 1;
+            }
+        }
+    }
+
+    //verifica as colunas
+    for(i = 0; i < 3; i++)
+    {
+        if(matrix[0][i] != ' ' && matrix[1][i] != ' ' && matrix[2][i] != ' ')
+        {
+            if(matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i])
+            {
+                return 1;
+            }
+        }
+    }
+
+    //verifica as diagonais
+    if(matrix[0][0] != ' ' && matrix[1][1] != ' ' && matrix[2][2] != ' ')
+    {
+        if(matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2])
+        {
+            return 1;
+        }
+    }
+
+    if(matrix[0][2] != ' ' && matrix[1][1] != ' ' && matrix[2][0] != ' ')
+    {
+        if(matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0])
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+//funcao que verifica o empate
+int check_draw()
 {
     return 0;
 }
